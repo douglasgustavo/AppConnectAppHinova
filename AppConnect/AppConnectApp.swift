ebada518c9ff93ac15fd61f778eea25cc788abef
Douglas Santos <douglas.gsantos@icloud.com>
2021-10-31 10:15:19 -0300

    Initial Commit

---
+++ AppConnect/AppConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppConnectApp.swift
+//  AppConnect
+//
+//  Created by Douglas Santos on 31/10/21.
+//
+
+import SwiftUI
+
+@main
+struct AppConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
